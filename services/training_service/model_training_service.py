f58fa09b85e07c9b65090b57e3bcc14cb1ec4c4a
primethanos <francis.neequaye@gmail.com>
2023-12-24 08:06:13 +0000

    ModelTrainingService

---
+++ services/training_service/model_training_service.py
@@ -0,0 +1,112 @@
+# services/training_service/model_training_service.py
+import torch
+import torch.optim as optim
+import torch.nn as nn
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+import time
+import logging
+from datetime import datetime
+from tqdm import tqdm
+
+
+class ModelTrainingService:
+    def __init__(self, model, train_loader, model_config, config, model_version_service,
+                 in_google_colab=False):
+
+        self.model = model
+        self.train_loader = train_loader
+        self.model_config = model_config
+        self.config = config
+        self.model_version_service = model_version_service
+        self.in_google_colab = in_google_colab
+
+        # Model loss function
+        self.criterion = nn.BCELoss() if self.model_config['loss'] == 'BCE' else nn.CrossEntropyLoss()
+
+        self.logger = logging.getLogger(__name__)
+        self.setup_logging()
+
+        # Optimizer setup
+        self.optimizer = optim.Adam(
+            self.model.parameters(),
+            lr=self.model_config.get('learning_rate', 0.001)
+        )
+
+    def setup_logging(self):
+        """Configure logging for the training service."""
+        logging_level = self.config.get('logging_level', logging.INFO)
+        logging_format = self.config.get('logging_format', '%(asctime)s - %(levelname)s - %(message)s')
+        if self.in_google_colab:
+            logging.basicConfig(level=logging_level, format=logging_format)
+        else:
+            log_filename = self.config.get('log_filename', 'training.log')
+            logging.basicConfig(level=logging_level, format=logging_format,
+                                filename=log_filename, filemode='a')
+
+    def train_model(self):
+        start_time = datetime.now()
+
+        # Retrieve model training parameters from model_config
+        epochs = self.model_config.get('epochs', 2)
+        checkpoint_interval = self.model_config.get('checkpoint_interval', 5)
+
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model.to(device)
+        self.logger.info(f"Model is on: {device}")
+
+        scheduler = ReduceLROnPlateau(self.optimizer, **self.model_config.get('scheduler_params', {}))
+
+        best_loss = float('inf')
+        for epoch in tqdm(range(epochs), desc="Training Epochs"):
+            self.model.train()  # Set model to training mode
+            epoch_loss = 0.0
+            for inputs, labels in self.train_loader:
+                inputs, labels = inputs.to(device), labels.to(device)
+                self.optimizer.zero_grad()
+                outputs = self.model(inputs)
+                outputs, labels = self.adjust_shapes(outputs, labels)
+                loss = self.criterion(outputs, labels)
+                loss.backward()
+                self.optimizer.step()
+                epoch_loss += loss.item() * inputs.size(0)
+
+            avg_epoch_loss = epoch_loss / len(self.train_loader.dataset)
+            scheduler.step(avg_epoch_loss)
+            self.logger.info(f"Epoch {epoch + 1}, Average Loss: {avg_epoch_loss}")
+
+            if avg_epoch_loss < best_loss:
+                best_loss = avg_epoch_loss
+                if epoch % checkpoint_interval == 0:
+                    self.save_checkpoint(epoch, best_loss)
+
+            # Early stopping condition based on scheduler's bad epochs count
+            if getattr(scheduler, 'num_bad_epochs', 0) == self.model_config.get('early_stopping_patience', 10):
+                self.logger.info("Early stopping triggered.")
+                break
+
+        training_time = (datetime.now() - start_time).total_seconds()
+        self.logger.info(f"Training completed in {training_time:.2f} seconds.")
+
+        return self.model, best_loss, len(self.train_loader.dataset), training_time
+
+    def adjust_shapes(self, outputs, labels):
+        """Adjust the labels and outputs dimensions if necessary."""
+        if self.model.num_classes == 1 and outputs.dim() > 1:
+            outputs = outputs.squeeze(1)
+        labels = labels.view_as(outputs)  # Ensure label shapes match output shapes
+        return outputs, labels
+
+    def save_checkpoint(self, epoch, loss):
+        """Save model checkpoint."""
+        # Path construction for checkpoint saving
+        model_save_path = self.config.get('model_save_path', './models/')
+        checkpoint_filename = f"model_epoch_{epoch}.pth"
+        path = f"{model_save_path}{checkpoint_filename}"
+
+        torch.save({
+            'epoch': epoch,
+            'model_state_dict': self.model.state_dict(),
+            'optimizer_state_dict': self.optimizer.state_dict(),
+            'loss': loss,
+        }, path)
+        self.logger.info(f"Checkpoint saved: {path}")
